bced3a7125aea7d7277d8ac85c1a670903450e02
SubadhaAuraxThvani <subadhaaura@gmail.com>
2024-12-02 15:20:09 +0530

    background video

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ import Home8 from "@/components/Home/Home8";
 export default function App() {
   return (
     <div className="overflow-x-hidden">
-      <video className="w-[100vw] h-[100vh] object-fill"  autoPlay loop >
+      <video className="w-[100vw] h-[100vh] object-fill"  autoPlay loop muted>
         <source src="/video/video.mp4" type="video/mp4" />
-        </video>
+      </video>
       <Home/>
       <Home2 props={"Fashion with Nature's Essence"} />
       <Home3 />
