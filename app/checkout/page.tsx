8730467b0cde40e0d3e587b69d5763cfeab6bc56
harris502126 <115565515+harris502126@users.noreply.github.com>
2025-01-13 16:53:19 +0530

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,14 @@
+"use client"
 import CheckOutPage from "@/components/CheckOutPage/Checkout";
+import { useSearchParams } from "next/navigation";
 
 export default function Check() {
+    const searchParams = useSearchParams();
+    const cart = searchParams.get("cartId");
+    console.log(cart)
     return (
-       <CheckOutPage/>
+        <CheckOutPage
+            cartId={cart}
+        />
     )
 }
